84530122bac454779b4296bad4afe97fe01332b1
Gimm17 <agimpratamazte@gmail.com>
2026-05-11 16:12:33 +0800

    fix: cast workspace_id to integer to resolve MySQL TypeError

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,6 +34,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
+            'workspace_id' => 'integer',
             'email_verified_at' => 'datetime',
             'last_login_at' => 'datetime',
             'password' => 'hashed',
